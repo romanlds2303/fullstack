1226840a709cb414d86ac22526ebeece2b4eb56c
Román Ledesma <romanledesmaa23@gmail.com>
2024-06-18 21:31:13 -0300

    Intro a js

---
+++ 04-introjavascript/index.js
@@ -0,0 +1,13 @@
+//estos son los comentarios de una sola linea en javascript
+
+let hola = "pepe"
+
+//variables: espacios de memoria para almacenar datos
+
+
+//1. con var
+// palabras reservadas: son utilizadas solo para las definiciones del lenguaje de programación-
+var nombre;
+var email
+var edad;
+var pais
