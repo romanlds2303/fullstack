7d1eb34b1709a5dcac63bf2adb33e856514f1f57
Román Ledesma <romanledesmaa23@gmail.com>
2024-06-25 21:20:38 -0300

    intro a js y variables

---
+++ 04-introjavascript/variables.js
@@ -0,0 +1,66 @@
+//estos son los comentarios de una sola linea en javascript
+
+let hola = "pepe"
+
+//variables: espacios de memoria para almacenar datos
+
+
+//1. con var
+// palabras reservadas: son utilizadas solo para las definiciones del lenguaje de programación-
+var nombre;
+var email
+var edad;
+var pais
+
+
+// no podemos comenzar el nombre de una variable con números/espacios en blannco/con simbolos
+/*
+var:1234;
+var nombre apellido;
+var =pais;
+var -edad;
+var nombre-apellido;
+*/
+
+
+console.log("3-ESTOY EN LA CABECERA DE LA PAGINA HTML")
+
+// Si ""
+var $provincia;
+var _nombre;
+var nombreApellidoPersona; //camelcase
+var nombre_apellido_persona; //underscore
+
+let provincia; 
+
+// const representa una constante: no cambia en la ejecucción del programa 
+
+// esto es una declaracion de una variable y asignacion de un valor
+const DNI = 12345678;
+
+nombre= "Juan"; //string
+// apellido= Perez; //me falta ponerle comillas simpes a perez
+apellido= "Pérez"
+
+// = representa asignacion de datos
+
+provincia= "Buenos Aires"
+
+provincia= "CABA"
+
+provincia= "Cordoba"
+
+provincia= 15; //numeros - int
+
+provincia= true; //boolean
+
+provincia= false;
+
+//funciones
+console.log(nombre+apellido+provincia);
+console.info(nombre+apellido+provincia);
+console.error(nombre+apellido+provincia);
+console.warn(nombre+apellido+provincia);
+
+
+alert("soy un mensaje de alerta")
